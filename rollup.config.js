b3189b3fb573d4a95fb216b0c9bcc950c00410b6
koladeolukoya <koladeolukoya@gmail.com>
2024-09-10 08:30:32 +0100

    feat: downgraded react-dnd added babel

--- rollup.config.js
+++ rollup.config.js
@@ -1,46 +1,64 @@
-import resolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
-import typescript from '@rollup/plugin-typescript';
-import { terser } from 'rollup-plugin-terser';
-import external from 'rollup-plugin-peer-deps-external';
-import postcss from 'rollup-plugin-postcss';
-import json from '@rollup/plugin-json';
-import alias from '@rollup/plugin-alias';
-const packageJson = require('./package.json');
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import typescript from "@rollup/plugin-typescript";
+import { terser } from "rollup-plugin-terser";
+import external from "rollup-plugin-peer-deps-external";
+import postcss from "rollup-plugin-postcss";
+import json from "@rollup/plugin-json";
+import alias from "@rollup/plugin-alias";
+import babel from "@rollup/plugin-babel";
+import dynamicImportVars from "@rollup/plugin-dynamic-import-vars";
+const packageJson = require("./package.json");
 
 export default {
-  input: './src/components/pages/editor.tsx',
+  input: "./src/components/pages/editor.tsx",
   output: [
     {
       file: packageJson.main,
-      format: 'cjs',
+      format: "cjs",
       sourcemap: true,
-      name: 'odoc_editor_v2',
+      name: "odoc_editor_v2",
       inlineDynamicImports: true,
+      exports: "auto",
     },
     {
       file: packageJson.module,
-      format: 'esm',
+      format: "esm",
       sourcemap: true,
       inlineDynamicImports: true,
-    }
+      esModule: true,
+    },
   ],
   plugins: [
     alias({
-      entries: [
-        { find: 'stream', replacement: 'readable-stream' }
-      ]
+      entries: [{ find: "stream", replacement: "readable-stream" }],
     }),
     json(),
     external(),
     resolve(),
     commonjs(),
-    typescript({ tsconfig: './tsconfig.json' }),
+    typescript({
+      tsconfig: "./tsconfig.json",
+      noEmit: true, // Add this line
+    }),
+    // babel({
+    //   presets: [
+    //     [
+    //       "@babel/preset-env",
+    //       {
+    //         modules: false, // Add this line
+    //       },
+    //     ],
+    //   ],
+    //   extensions: [".ts", ".tsx"],
+    //   babelHelpers: "bundled", // Add this line
+    // }),
     postcss(),
-    terser()
+    dynamicImportVars(),
+    terser(),
   ],
   external: [
     ...Object.keys(packageJson.peerDependencies || {}),
-    ...Object.keys(packageJson.dependencies || {})
-  ]
+    ...Object.keys(packageJson.dependencies || {}),
+  ],
 };
